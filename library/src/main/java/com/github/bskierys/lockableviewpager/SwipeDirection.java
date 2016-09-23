99fa3df2d0f1ca74d72f3d10e6b5741e99ce88c2
bartlomiej.kierys <bskierys@gmail.com>
2016-09-23 09:10:10 +0200

    Changed package, project name and moule names to match bintray and mavenCentral

---
+++ library/src/main/java/com/github/bskierys/lockableviewpager/SwipeDirection.java
@@ -0,0 +1,16 @@
+/*
+* author: Bartlomiej Kierys
+* date: 2016-09-15
+* email: bskierys@gmail.com
+*/
+package com.github.bskierys.lockableviewpager;
+
+/**
+ * TODO: Generic description. Replace with real one.
+ */
+public enum SwipeDirection {
+    ALL,
+    LEFT,
+    RIGHT,
+    NONE
+}
---
+++ sample/src/main/java/com/github/bskierys/sample/MainActivity.java
@@ -0,0 +1,81 @@
+package com.github.bskierys.sample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.github.bskierys.lockableviewpager.LockableViewPager;
+import com.github.bskierys.lockableviewpager.SwipeDirection;
+import com.github.bskierys.lockableviewpager.sample.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final String[] PAGE_NAMES = {"menzurka", "paradygmat", "karmelitka", "terpentyna", "zalewajka"};
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter pagerAdapter;
+
+    @BindView(R.id.block_view_pager) LockableViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    @OnClick(R.id.btn_block_left) public void onBlockLeft(Button self) {
+        if (viewPager.isLocked(SwipeDirection.LEFT)) {
+            viewPager.unlock(SwipeDirection.LEFT);
+            self.setText("LOCK LEFT");
+        } else {
+            viewPager.lock(SwipeDirection.LEFT);
+            self.setText("UNLOCK LEFT");
+        }
+    }
+
+    @OnClick(R.id.btn_block_right) public void onBlockRight(Button self) {
+        if (viewPager.isLocked(SwipeDirection.RIGHT)) {
+            viewPager.unlock(SwipeDirection.RIGHT);
+            self.setText("LOCK RIGHT");
+        } else {
+            viewPager.lock(SwipeDirection.RIGHT);
+            self.setText("UNLOCK RIGHT");
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ScreenSlidePageFragment.newInstance(PAGE_NAMES[position]);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_NAMES.length;
+        }
+    }
+}
